0d7db47b756d453e81239221a7e94596576f4c71
Marcell Mars <ki.ber@kom.uni.st>
2013-03-18 13:13:59 +0100

    let's try the same start/kill lines for osx.

--- letssharebooks.sh
+++ letssharebooks.sh
@@ -5,8 +5,10 @@ echo " http://localhost:8080 (help: http://bit.ly111IWwt)"
 echo " Hang out at http://crypto.cat room: letssharebooks"
 echo " Stop sharing books by pressing Ctrl+c"
 
-# uncomment these lines + the two lines at the end to start and kill calibre-server from this script
-#calibre-server --max-cover=300x400 --daemonize --pidfile=/tmp/calibre.pid -p 8080
+# uncomment these lines + the two lines at the end to start and kill calibre-server from this script if on linux:
+#calibre-server --max-cover=300x400 --daemonize --pidfile=/tmp/calibre.pid h-p 8080
+# or this one + the two lines at the end if on osx:
+#/Applications/calibre.app/Contents/MacOS/calibre-server --max-cover=300x400 --daemonize --pidfile=/tmp/calibre.pid h-p 8080
 
 exec 3>&1
 ssh -N -T ssh.memoryoftheworld.org -l tunnel -R 0:localhost:8080 -p 722 2>&1 1>&3 | sed 's|^Allocated port \([[:digit:]]\{4,5\}\)\(.*\)| Your temporary public URL is http://www\1.memoryoftheworld.org|'

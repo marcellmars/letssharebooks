a902f7d09c5be4a297a66368beec1af49cf5db7d
Marcell Mars <ki.ber@kom.uni.st>
2013-03-19 02:51:35 +0100

    if calibre-server used from let's share books it should do now sane kill. added windows version.

---
+++ windows/lsb.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+echo Check if you started Calibre's content server:
+echo http://localhost:8080 (help: http://bit.ly111IWwt)
+echo Hang out at http://crypto.cat room: letssharebooks
+echo Stop sharing books by pressing Ctrl+c
+
+set /a port=%random% %%40000 +1000
+echo http://www%port%.memoryoftheworld.org
+
+plink.exe -N -T tunnel@ssh.memoryoftheworld.org -R %port%:localhost:8080 -P 722
+

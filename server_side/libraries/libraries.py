400f13cd89694a1396c22cdfc4d8b7411a67f760
marcell mars <ki.ber@kom.uni.st>
2013-09-05 05:09:03 +0200

    search with autocomplete completed. should fix unicode in input fields.

--- server_side/libraries/libraries.py
+++ server_side/libraries/libraries.py
@@ -6,6 +6,7 @@ import md5
 import glob
 import simplejson
 import operator
+import itertools
 
 class JSONBooks:
     def __init__(self, domain = "web.dokr"):
@@ -15,24 +16,24 @@ class JSONBooks:
         uid = subprocess.check_output(["grep", "{0}".format(login), "/etc/passwd"]).split()[0].split(":")[2]
         return subprocess.check_output(["/usr/local/bin/get_tunnel_ports.sh", uid]).split()
 
-    def get_metadata(self, start, offset):
+    def get_metadata(self, start, offset, query):
         end = start + offset
         all_books = []
         for tunnel in self.get_tunnel_ports():
             base_url = 'http://www{tunnel}.{domain}/'.format(tunnel=tunnel, domain=self.domain)
             total_num_url = 'ajax/search?query='
             total_num = requests.get("{base_url}{total_num_url}".format(base_url=base_url, total_num_url=total_num_url)).json()['total_num']
-            books_ids_url = 'ajax/search?query=&num={total_num}&sort=last_modified'.format(total_num=total_num)
+            books_ids_url = 'ajax/search?query={query}&num={total_num}&sort=last_modified'.format(query=query, total_num=total_num)
             books_ids = requests.get("{base_url}{books_ids_url}".format(base_url=base_url, books_ids_url=books_ids_url)).json()['book_ids']
             books_ids_hash = md5.new("".join((str(book_id) for book_id in books_ids))).hexdigest()
-            hash_files = glob.glob("{books_ids_hash}_*".format(books_ids_hash=books_ids_hash))
-            hash_filename = "{books_ids_hash}_{tunnel}".format(books_ids_hash=books_ids_hash, tunnel=tunnel)
+            hash_files = glob.glob("hashfiles/{books_ids_hash}_*".format(books_ids_hash=books_ids_hash))
+            hash_filename = "hashfiles/{books_ids_hash}_{tunnel}".format(books_ids_hash=books_ids_hash, tunnel=tunnel)
             if hash_files:
                 if hash_filename in hash_files:
-                    books = simplejson.loads(open(hash_filename, "r").read())
+                    books = simplejson.loads(open("{hash_filename}".format(hash_filename=hash_filename), "r").read())
                     all_books = books
                 else:
-                    books = simplejson.loads(open(glob.glob("{books_ids_hash}_*".format(books_ids_hash=books_ids_hash))[0, "r"]).read())
+                    books = simplejson.loads(open(glob.glob("hashfiles/{books_ids_hash}_*".format(books_ids_hash=books_ids_hash))[0, "r"]).read())
                     for book in books:
                         book['tunnel'] = tunnel
                     all_books = books
@@ -50,33 +51,26 @@ class JSONBooks:
                     book['formats'] = book_metadata['formats']
                     all_books.append(book)
 
-            open(hash_filename, "w").write(simplejson.dumps(all_books))
+            open("{hash_filename}".format(hash_filename=hash_filename), "w").write(simplejson.dumps(all_books))
         all_books.sort(key=operator.itemgetter('title_sort'))
-        return all_books[start:end]
+        authors_key = operator.itemgetter("authors")
+        toolbar_authors = sorted(list(set(list(itertools.chain.from_iterable(map(authors_key, all_books))))))
+        titles_key = operator.itemgetter("title")
+        toolbar_titles = sorted(list(set(map(titles_key, all_books))))
+        toolbar_data = {"total_num": len(all_books), "authors": toolbar_authors, "titles": toolbar_titles}
+        all_books_return = all_books[start:end]
+        all_books_return.append(toolbar_data)
+        return all_books_return
 
 class Root(object):
-
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    @cherrypy.tools.json_in()
-    def flip_page(self):
-        #cl = cherrypy.request.headers['Content-Length']
-        #body = cherrypy.request.body.read(int(cl))
-        print(cherrypy.request.json)
-        return cherrypy.request.json
-        #return body
-
  
     @cherrypy.expose
     @cherrypy.tools.json_out()
     @cherrypy.tools.json_in()
     def render_page(self):
         json_books = JSONBooks()
-        start_offset = cherrypy.request.json
-        print(start_offset['start'])
-        #cl = cherrypy.request.headers['Content-Length']
-        #body = cherrypy.request.body.read(int(cl))
-        return json_books.get_metadata(start_offset['start'], start_offset['offset'])
+        json_request = cherrypy.request.json
+        return json_books.get_metadata(json_request['start'], json_request['offset'], json_request['query'])
 
     @cherrypy.expose
     def index(self):
@@ -91,13 +85,15 @@ class Root(object):
 <script type='text/javascript'>
 var LSB = {}
 LSB.start = 0;
+LSB.total_num = 1;
 LSB.offset = 8;
+LSB.query = "";
+LSB.carry = "";
 
 init_page = function() {
                 add_toolbar();
                 render_page();
 }
-
 
 render_page = function() {
 
@@ -108,6 +104,13 @@ render_page = function() {
       processData: false,
       data: JSON.stringify(LSB),
       success: function(books) {
+                    toolbar_data = books.pop()
+                    LSB.total_num = toolbar_data['total_num']
+                    refresh_pagination()
+                    $(function () {
+                            $('#authors').autocomplete({source: toolbar_data['authors']});
+                            $('#titles').autocomplete({source: toolbar_data['titles']});
+                        });
                     $.each(books, function(n, book) {
                         var base_url = 'http://www' + book.tunnel + '.' + book.domain
                         var formats = ""
@@ -121,16 +124,60 @@ render_page = function() {
     });
 }
 
+refresh_pagination = function () {
+    $('.pagination').button({label: ' ' + LSB.start + ' - ' + (+LSB.start + +LSB.offset) + ' out of ' + LSB.total_num + ' books ', disabled: true})
+}
+
 add_toolbar = function() {
     $('#content').append('<div id="toolbar"><div id="prev_page"></div><div id="next_page"></div></div>');
-    $('#toolbar').append($('#prev_page').button({label: 'prev'}))
+    $('#content').append('<div id="searchbar"></div>')
+
+    $('#toolbar').append($('#prev_page').button({label: 'prev'}));
     $('#prev_page').click(function() {prev_page()});
-    $('#toolbar').append($('#next_page').button({label: 'next'})).click(function() {next_page()});
+    $('#toolbar').append($('#next_page').button({label: 'next'}));
     $('#next_page').click(function() {next_page()});
+    $('#toolbar').append('<div class="pagination"></div>');
+
+    $('#searchbar').append('<div class="ui-widget"><input id="authors" placeholder="authors"/><input id="titles" placeholder="titles"/><input id="search_all" placeholder="search all"/></div><div id="search"></div>')
+    $('#search').button({label: 'search'}).click(search_query)
+
+}
+
+search_query = function () {
+    if ($('#authors').val()) {
+        LSB.query = 'authors:' + $('#authors').val()
+        LSB.carry = " OR "
+    } else {
+        LSB.query = '';
+        LSB.carry = '';
+    }
+    if ($('#titles').val()) {
+        LSB.query += LSB.carry + 'title:' + $('#titles').val()
+        LSB.carry = " OR "
+    } else if (LSB.carry != " OR ") {
+        LSB.query = '';
+        LSB.carry = '';
+    }
+
+    if ($('#search_all').val()) {
+        LSB.query += LSB.carry + $('#search_all').val()
+    } else if (LSB.carry != " OR ") {
+        LSB.query = '';
+        LSB.carry = '';
+    }
+    
+    console.log(LSB.query)
+    LSB.start = 0;
+    $('#content').empty();
+    add_toolbar()
+    render_page()
 }
 
 next_page = function() {
     LSB.start = LSB.start + LSB.offset;
+    if (LSB.start+LSB.offset >= LSB.total_num) {
+        LSB.start = LSB.total_num - LSB.offset;
+    }
     $('#content').empty();
     add_toolbar();
     render_page();
@@ -138,6 +185,9 @@ next_page = function() {
 
 prev_page = function() {
     LSB.start = LSB.start - LSB.offset;
+    if (LSB.start <= 0) {
+        LSB.start = 0;
+    }
     $('#content').empty();
     add_toolbar();
     render_page();

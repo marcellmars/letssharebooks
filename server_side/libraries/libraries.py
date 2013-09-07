5c35616f10ce37115c20a451bedbd1fe936995ae
marcell mars <ki.ber@kom.uni.st>
2013-09-08 00:37:38 +0200

    added custom jquery ui. moved new libraries to threads with message about new books coming.

--- server_side/libraries/libraries.py
+++ server_side/libraries/libraries.py
@@ -7,6 +7,46 @@ import glob
 import simplejson
 import operator
 import itertools
+import threading
+import time
+
+class UrlLibThread(threading.Thread):
+    def __init__(self, books_ids, book_metadata_url, domain, tunnel, books_ids_hash, base_url):
+        threading.Thread.__init__(self)
+        self.books_ids = books_ids
+        self.base_url = base_url
+        self.book_metadata_url = book_metadata_url
+        self.domain = domain
+        self.tunnel = tunnel
+        self.books_ids_hash = books_ids_hash
+        self.hash_filename = "{base_dir}hashfiles/{books_ids_hash}_{tunnel}".format(base_dir=base_dir, books_ids_hash=self.books_ids_hash, tunnel=self.tunnel)
+
+    def run(self):
+        all_books = []
+        processing_file_name = "{base_dir}hashfiles/_processing_{books_ids_hash}_{tunnel}".format(base_dir=base_dir, books_ids_hash=self.books_ids_hash, tunnel=self.tunnel)
+
+        if os.path.isfile(processing_file_name):
+            pass
+        else:
+            with open(processing_file_name, "w") as f:
+                f.write("")
+
+            for book_id in self.books_ids:
+                book = {}
+                book_metadata = requests.get("{base_url}{book_metadata_url}{book_id}".format(base_url=self.base_url, book_metadata_url=self.book_metadata_url, book_id=book_id)).json()
+                book['id'] = book_id
+                book['tunnel'] = self.tunnel
+                book['title'] = book_metadata['title']
+                book['title_sort'] = book_metadata['title_sort']
+                book['authors'] = book_metadata['authors']
+                book['domain'] = self.domain
+                book['formats'] = book_metadata['formats']
+                all_books.append(book)
+            os.remove(processing_file_name) 
+            with open("{hash_filename}".format(hash_filename=self.hash_filename), "w") as f:
+                f.write(simplejson.dumps(all_books))
+            return
+ 
 
 class JSONBooks:
     def __init__(self, domain = "web.dokr"):
@@ -16,52 +56,69 @@ class JSONBooks:
         uid = subprocess.check_output(["grep", "{0}".format(login), "/etc/passwd"]).split()[0].split(":")[2]
         return subprocess.check_output(["/usr/local/bin/get_tunnel_ports.sh", uid]).split()
 
+    def search_metadata(self):
+        with open(self.hash_filename, "r") as f:
+            books_hash = simplejson.loads(f.read())
+
+        for book_id in self.search_books_ids:
+            self.all_books.append([book for book in books_hash if book['id'] == book_id][0])
+
     def get_metadata(self, start, offset, query):
-        query = query.encode('utf-8')
-        end = start + offset
-        all_books = []
+        processing_status = ""
+        self.tunnel = False
+        self.query = query.encode('utf-8')
+        self.start = start
+        self.offset = offset
+        self.end = self.start + self.offset
+        self.all_books = []
         for tunnel in self.get_tunnel_ports():
-            base_url = 'http://www{tunnel}.{domain}/'.format(tunnel=tunnel, domain=self.domain)
-            total_num_url = 'ajax/search?query={query}'.format(query=query)
-            total_num = requests.get("{base_url}{total_num_url}".format(base_url=base_url, total_num_url=total_num_url)).json()['total_num']
-            if total_num == 0:
+            self.tunnel = tunnel
+            self.base_url = 'http://www{tunnel}.{domain}/'.format(tunnel=self.tunnel, domain=self.domain)
+            self.total_num_url = 'ajax/search?query='
+            self.total_num = requests.get("{base_url}{total_num_url}".format(base_url=self.base_url, total_num_url=self.total_num_url)).json()['total_num']
+            if self.total_num == 0:
                 continue
-            books_ids_url = 'ajax/search?query={query}&num={total_num}&sort=last_modified'.format(query=query, total_num=total_num)
-            books_ids = requests.get("{base_url}{books_ids_url}".format(base_url=base_url, books_ids_url=books_ids_url)).json()['book_ids']
-            books_ids_hash = md5.new("".join((str(book_id) for book_id in books_ids))).hexdigest()
-            hash_files = glob.glob("{base_dir}hashfiles/{books_ids_hash}_*".format(base_dir=base_dir, books_ids_hash=books_ids_hash))
-            hash_filename = "{base_dir}hashfiles/{books_ids_hash}_{tunnel}".format(base_dir=base_dir, books_ids_hash=books_ids_hash, tunnel=tunnel)
-            if hash_files:
-                if hash_filename in hash_files:
-                    books = simplejson.loads(open("{hash_filename}".format(hash_filename=hash_filename), "r").read())
-                    all_books = books
+            self.books_ids_url = 'ajax/search?query=&num={total_num}&sort=last_modified'.format(total_num=self.total_num)
+            self.books_ids = requests.get("{base_url}{books_ids_url}".format(base_url=self.base_url, books_ids_url=self.books_ids_url)).json()['book_ids']
+            self.book_metadata_url = 'ajax/book/'
+            self.first_uuid = requests.get("{base_url}{book_metadata_url}{book_id}".format(base_url=self.base_url, book_metadata_url=self.book_metadata_url, book_id=self.books_ids[0])).json()['uuid']
+            self.books_ids_hash = md5.new("{first_uuid}{books_ids_string}".format(first_uuid=self.first_uuid, books_ids_string="".join((str(book_id) for book_id in self.books_ids)))).hexdigest()
+            self.hash_filename = "{base_dir}hashfiles/{books_ids_hash}_{tunnel}".format(base_dir=base_dir, books_ids_hash=self.books_ids_hash, tunnel=self.tunnel)
+
+            if self.query != "":
+                self.search_query = 'ajax/search?query={query}'.format(query=self.query)
+                self.search_books_ids = requests.get("{base_url}{search_query}&sort=title".format(base_url=self.base_url, search_query=self.search_query)).json()['book_ids']
+                self.search_metadata()
+                continue
+
+            self.hash_files = glob.glob("{base_dir}hashfiles/{books_ids_hash}_*".format(base_dir=base_dir, books_ids_hash=self.books_ids_hash))
+            if self.hash_files:
+                if self.hash_filename in self.hash_files:
+                    with open(self.hash_filename, "r") as f:
+                        books = simplejson.loads(f.read())
+                    [self.all_books.append(book) for book in books]
                 else:
-                    books = simplejson.loads(open(glob.glob("{base_dir}hashfiles/{books_ids_hash}_*".format(base_dir=base_dir, books_ids_hash=books_ids_hash))[0], "r").read())
+                    with open(glob.glob("{base_dir}hashfiles/{books_ids_hash}_*".format(base_dir=base_dir, books_ids_hash=self.books_ids_hash))[0], "r") as f:
+                        books = simplejson.loads(f.read())
                     for book in books:
                         book['tunnel'] = tunnel
-                    all_books = books
+                    [self.all_books.append(book) for book in books]
+                    with open(self.hash_filename, "w") as f:
+                        f.write(simplejson.dumps(self.all_books))
             else:
-                book_metadata_url = 'ajax/book/'
-                for book_id in books_ids:
-                    book = {}
-                    book_metadata = requests.get("{base_url}{book_metadata_url}{book_id}".format(base_url=base_url, book_metadata_url=book_metadata_url, book_id=book_id)).json()
-                    book['id'] = book_id
-                    book['tunnel'] = tunnel
-                    book['title'] = book_metadata['title']
-                    book['title_sort'] = book_metadata['title_sort']
-                    book['authors'] = book_metadata['authors']
-                    book['domain'] = self.domain
-                    book['formats'] = book_metadata['formats']
-                    all_books.append(book)
-
-            open("{hash_filename}".format(hash_filename=hash_filename), "w").write(simplejson.dumps(all_books))
-        all_books.sort(key=operator.itemgetter('title_sort'))
+                thrd = UrlLibThread(self.books_ids, self.book_metadata_url, self.domain, self.tunnel, self.books_ids_hash, self.base_url)
+                thrd.start()
+                processing_status = " + atm, few new books are coming..."
+                #thrd.join()
+        self.all_books.sort(key=operator.itemgetter('title_sort'))
         authors_key = operator.itemgetter("authors")
-        toolbar_authors = sorted(list(set(list(itertools.chain.from_iterable(map(authors_key, all_books))))))
+        toolbar_authors = sorted(list(set(list(itertools.chain.from_iterable(map(authors_key, self.all_books))))))
         titles_key = operator.itemgetter("title")
-        toolbar_titles = sorted(list(set(map(titles_key, all_books))))
-        toolbar_data = {"total_num": len(all_books), "authors": toolbar_authors, "titles": toolbar_titles}
-        all_books_return = all_books[start:end]
+        toolbar_titles = sorted(list(set(map(titles_key, self.all_books))))
+        if glob.glob("{base_dir}hashfiles/_processing_*".format(base_dir=base_dir)):
+            processing_status = " + atm, few new books are coming..."
+        toolbar_data = {"total_num": len(self.all_books), "authors": toolbar_authors, "titles": toolbar_titles, "query": self.query, "processing": processing_status}
+        all_books_return = self.all_books[self.start:self.end]
         all_books_return.append(toolbar_data)
         return all_books_return
 
@@ -80,18 +137,18 @@ class Root(object):
         return """
 <html>
 <head><title>Libraries</title>
-<script type="application/javascript" src="static/jquery-1.10.2.min.js"></script>
-<script type="application/javascript" src="static/jquery-1.10.2.min.map"></script>
-<script type="application/javascript" src="static/jquery-ui.js"></script>
+<script type="application/javascript" src="static/jquery-1.9.1.js"></script>
+<script type="application/javascript" src="static/jquery-ui-1.10.3.custom.min.js"></script>
 <link rel="stylesheet" type="text/css" href="static/style.css" />
-<link rel="stylesheet" type="text/css" href="static/jquery-ui.css" />
-<script type='text/javascript'>
+<link rel="stylesheet" type="text/css" href="static/jquery-ui-1.10.3.custom.min.css" />
+<script type='text/javascript' charset='utf-8'>
 var LSB = {}
 LSB.start = 0;
 LSB.total_num = 1;
 LSB.offset = 8;
 LSB.query = "";
 LSB.carry = "";
+LSB.processing = "";
 
 
 init_page = function() {
@@ -112,6 +169,8 @@ render_page = function() {
                     add_toolbar();
                     toolbar_data = books.pop()
                     LSB.total_num = toolbar_data['total_num']
+                    LSB.query = toolbar_data['query']
+                    LSB.processing = toolbar_data['processing']
                     refresh_pagination()
                     $(function () {
                             $('#authors').autocomplete({source: toolbar_data['authors']});
@@ -132,30 +191,37 @@ render_page = function() {
 
                         last_comma = authors.lastIndexOf(",");
                         authors = authors.substr(0, last_comma) + authors.substr(last_comma + 1) + '</div>'
-                        console.log(authors)
-                        $('#content').append('<div class="cover"><a href="'+ base_url +'/browse/book/'+ book.id +'" target="_blank"><img src="' + base_url + '/get/cover/' + book.id + '.jpg"></img></a><h2>' + book.title + '<br/>' + authors  + '</h2><span class="download">Metadata: <a href="'+ base_url + '/get/opf/' + book.id  + book.title.replace(/\?/g, "") + '.opf">.opf</a><br/>Download: ' + formats + ' </span></div>')
+                        $('#content').append('<div class="cover"><a href="'+ base_url +'/browse/book/'+ book.id +'" target="_blank" id="more_about"><img src="' + base_url + '/get/cover/' + book.id + '.jpg"></img></a><h2>' + book.title + '<br/>' + authors  + '</h2><span class="download">Metadata: <a href="'+ base_url + '/get/opf/' + book.id  + book.title.replace(/\?/g, "") + '.opf">.opf</a><br/>Download: ' + formats + ' </span></div>')
                 });
+
+                    $('.cover').each(function() { 
+                                $(this).click(function() {
+                                    window.open($(this).children('a:first').attr('href'));
+                                    return false;
+                                    });
+                            });
     },
       dataType: "json"
     });
 }
 
 refresh_pagination = function () {
-    $('.pagination').button({label: ' ' + (+LSB.start + 1)+ ' - ' + (+LSB.start + +LSB.offset) + ' out of ' + LSB.total_num + ' books ', disabled: true})
+    //$('.pagination').button({label: ' ' + (+LSB.start + 1)+ ' - ' + (+LSB.start + +LSB.offset) + ' out of ' + LSB.total_num + ' books ' + LSB.processing, disabled: true})
+    $('.pagination').button({label: 'HOME (' + (+LSB.start + 1)+ ' - ' + (+LSB.start + +LSB.offset) + ' out of ' + LSB.total_num + ' books)' + LSB.processing}).click(function() {search_query()})
 }
 
 add_toolbar = function() {
     $('#content').append('<div id="toolbar"><div id="prev_page"></div><div id="next_page"></div></div>');
     $('#content').append('<div id="searchbar"></div>')
 
-    $('#toolbar').append($('#prev_page').button({label: 'prev'}));
+    $('#toolbar').append($('#prev_page').button({label: '<<<'}));
     $('#prev_page').click(function() {prev_page()});
-    $('#toolbar').append($('#next_page').button({label: 'next'}));
+    $('#toolbar').append($('#next_page').button({label: '>>>'}));
     $('#next_page').click(function() {next_page()});
     $('#toolbar').append('<div class="pagination"></div>');
 
     $('#searchbar').append('<div class="ui-widget"><input id="authors" placeholder="authors"/><input id="titles" placeholder="titles"/><input id="search_all" placeholder="search all metadata"/><div id="search"></div></div>')
-    $('#search').button({label: 'search'}).click(search_query)
+    $('#search').button({label: 'SEARCH'}).click(search_query)
 
 }
 

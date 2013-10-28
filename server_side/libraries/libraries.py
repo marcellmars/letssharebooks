fd873bf2768becbde688a05a4aa244e7a68812ef
marcell mars <ki.ber@kom.uni.st>
2013-10-27 21:15:47 -0400

    not finished.

--- server_side/libraries/libraries.py
+++ server_side/libraries/libraries.py
@@ -11,13 +11,15 @@ import itertools
 import threading
 import time
 import uuid
+import random
 from pymongo import MongoClient
 import pymongo
 from jinja2 import Environment, FileSystemLoader
 
 class UrlLibThread(threading.Thread):
-    def __init__(self, book_metadata_url, domain, tunnel, base_url):
+    def __init__(self, books_ids, book_metadata_url, domain, tunnel, base_url):
         threading.Thread.__init__(self)
+        self.books_ids = books_ids
         self.base_url = base_url
         self.book_metadata_url = book_metadata_url
         self.domain = domain
@@ -48,14 +50,14 @@ class UrlLibThread(threading.Thread):
         except requests.exceptions.RequestException as e:
             return False
 
-    def insert_new_book(self, book_id, library_uuid, bookk={}):
+    def insert_new_book(self, book_id, library_uuid):
         book = {}
-        if bookk != {}:
-            book_metadata = bookk
-        else:
-            book_metadata = self.get_book_metadata(book_id)
-            if not book_metadata:
-                return
+        book_metadata = self.get_book_metadata(book_id)
+        
+        if not book_metadata:
+            print("lock: {}".format(Db.lock.find_one({'library_uuid': self.tunnel + self.books_ids})['lock']))
+            Db.lock.update({'library_uuid': self.tunnel + self.books_ids}, {'$set': {'lock': 'False'}})
+            return
        
         book['id'] = book_id
         book['domain'] = self.domain
@@ -74,59 +76,71 @@ class UrlLibThread(threading.Thread):
         
         Db.books.update({'uuid': book_metadata['uuid']}, book, upsert=True)
         if book['uuid'] not in Db.libraries.distinct('book_uuids'):
-            Db.libraries.update({'library_uuid': library_uuid}, {'$push': {'book_uuids' : book['uuid'], 'books_ids' : book['id']}}, upsert=True)
+            Db.libraries.update({'library_uuid': library_uuid}, {'$push': {'book_uuids' : book['uuid'], 'uploaded_books_ids' : book['id']}}, upsert=True)
     
     def insert_new_books(self, new_books_ids, library_uuid):
-        Db.new_books_ids_proxy.update({'library_uuid': library_uuid}, {'$set':{'new_books_ids': new_books_ids}}, upsert=True)
-        while Db.new_books_ids_proxy.find_one({'library_uuid': library_uuid}, {'new_books_ids': {'$slice': -1}})['new_books_ids'] != []:
-            book_id = Db.new_books_ids_proxy.find_one({'library_uuid': library_uuid}, {'new_books_ids':{'$slice': -1}})['new_books_ids'][0]
-            Db.new_books_ids_proxy.update({'library_uuid': library_uuid}, {'$pop':{'new_books_ids': 1}})
+        Db.libraries.update({'library_uuid': library_uuid}, {'$set':{'books_ids': new_books_ids}}, upsert=True)
+        Db.lock.update({'library_uuid': self.tunnel + self.books_ids}, {'$set': {'lock': 'True'}}, upsert=True)
+        print("lock: {}".format(Db.lock.find_one({'library_uuid': self.tunnel + self.books_ids})['lock']))
+        
+        for book_id in new_books_ids:
             self.insert_new_book(book_id, library_uuid)
+        
+        Db.lock.update({'library_uuid': self.tunnel + self.books_ids}, {'$set': {'lock': 'False'}})
+
 
     def run(self):
-        library_uuid = str(uuid.uuid4())
-        while Db.books_ids_proxy.find_one({'tunnel': self.tunnel}, {'books_ids': {'$slice': 1}})['books_ids'] != []:
-            books_ids = Db.books_ids_proxy.find_one({'tunnel': self.tunnel})['books_ids']
-            #books_ids.reverse()
-            book_id = Db.books_ids_proxy.find_one({'tunnel': self.tunnel}, {'books_ids':{'$slice':1}})['books_ids'][0]
+        try:
+            if Db.lock.find_one({'library_uuid': self.tunnel + self.books_ids})['lock'] == 'True':
+                return
+        except:
+            Db.lock.update({'library_uuid': self.tunnel + self.books_ids}, {'$set': {'lock': 'False'}})
+
+        for book_id in range(4):
+            book_id = random.randint(0, len(self.books_ids))
             book = self.get_book_metadata(book_id)
 
             if not book:
                 return
+            
+            libraries = [library for library in Db.libraries.find({'book_uuids': {'$in': [book['uuid']]}})]
+            print("libraries: {}".format(libraries))
+            if libraries:
+                for library in libraries:
+                    Db.libraries.update({'library_uuid': libraries[0]['library_uuid']}, {'$addToSet': {'books_ids': {'$each': library['books_ids']}, 'book_uuids': {'$each': library['book_uuids']}}}, upsert=False)
+                    if library['library_uuid'] != libraries[0]['library_uuid']:
+                        Db.libraries.remove({'library_uuid': library['library_uuid']})
 
-            Db.books_ids_proxy.update({'tunnel': self.tunnel}, {'$pop': {'books_ids': -1}})
-            library = Db.libraries.find_one({'book_uuids': {'$in': [book['uuid']]}})
-            if library:
-                library_uuid = library['library_uuid']
+                library_uuid = libraries[0]['library_uuid']
                 if Db.books.find_one({'uuid': book['uuid']})['tunnel'] != self.tunnel:
                     #Db.books.update({'uuid': book['uuid']}, {'$set' : {'tunnel': self.tunnel}}, upsert=False)
                     for ujid in Db.libraries.find({'library_uuid': library_uuid }).distinct('book_uuids'):
                         Db.books.update({'uuid': ujid}, {'$set': {'tunnel': self.tunnel}}, upsert=False, multi=True)
 
                 self.seqs = []
-                print("books_ids_proxy: {} {} ; length={}".format(books_ids[0:10], books_ids[-10:],len(books_ids)))
-                print("library['books_ids']: {} {}; length={}".format(library['books_ids'][0:10], library['books_ids'][-10:], len(library['books_ids'])))
+                print("books_ids_proxy: {} {} ; length={}".format(self.books_ids[0:10], self.books_ids[-10:],len(self.books_ids)))
                 lib_books_ids = library['books_ids']
+                print("library['books_ids']: {} {}; length={}".format(library['books_ids'][0:10], library['books_ids'][-10:], len(library['books_ids'])))
                 #lib_books_ids.reverse()
-                seqz = self.compare_lists(lib_books_ids, books_ids)
+                seqz = self.compare_lists(lib_books_ids, self.books_ids)
                 old_books_ids = list(itertools.chain.from_iterable(seqz))
-                print("old_books_ids: {} {}".format(old_books_ids[0:10],old_books_ids[-10:] ))
-                lib_new_books_ids = Db.libraries.find_one({'library_uuid' : library_uuid})['books_ids']
-                new_books_ids = [book_id for book_id in books_ids if book_id not in set(old_books_ids) or book_id not in set(lib_new_books_ids)]
+                print("old_books_ids: {} {}".format(old_books_ids[0:10], old_books_ids[-10:]))
+                lib_new_books_ids = Db.libraries.find_one({'library_uuid': library_uuid})['uploaded_books_ids']
+                print("lib_new_books_ids: {} {}".format(lib_new_books_ids[0:10], lib_new_books_ids[-10:]))
+                new_books_ids = [book_id for book_id in self.books_ids if book_id not in set(old_books_ids) or book_id not in set(lib_new_books_ids)]
                 print("new_books_ids: {} {}".format(new_books_ids[0:10], new_books_ids[-10:]))
-                removed_books_ids = [book_id for book_id in library['books_ids'] if book_id not in set(books_ids)]
+                removed_books_ids = [book_id for book_id in library['books_ids'] if book_id not in set(self.books_ids)]
                 print("removed_books_ids: {}".format(removed_books_ids))
                 for book_id in removed_books_ids:
                     book_uuid = Db.books.find_one({'library_uuid' : library_uuid, 'id' : book_id})['uuid']
-                    Db.libraries.update({'library_uuid': library_uuid}, {'$pull': {'books_ids' : book_id, 'book_uuids': book_uuid}})
+                    Db.libraries.update({'library_uuid': library_uuid}, {'$pull': {'uploaded_books_ids' : book_id, 'book_uuids': book_uuid, 'books_ids': book_id}})
                     Db.books.remove({'uuid': book_uuid})
                 if new_books_ids != []:
                     self.insert_new_books(new_books_ids, library_uuid)
-                #Db.libraries.update({'library_uuid': library_uuid}, {'$set' : {'books_ids' : books_ids}})
-                return
-            else:
-                self.insert_new_book(book_id, library_uuid, bookk=book)
-        return
+                Db.libraries.update({'library_uuid': library_uuid}, {'$set' : {'books_ids' : self.books_ids}})
+                break
+        print("get new books!")
+        self.insert_new_books(self.books_ids, str(uuid.uuid4()))
 
 
 class JSONBooks:
@@ -177,12 +191,14 @@ class JSONBooks:
             if books_ids:
                 active_tunnels.append(tunnel)
                 Db.books_ids_proxy.update({'tunnel': tunnel}, {'$set':{'books_ids': books_ids}}, upsert=True)
-                thrd = UrlLibThread(book_metadata_url, self.domain, tunnel, base_url)
+                thrd = UrlLibThread("".join(map(str, books_ids)), book_metadata_url, self.domain, tunnel, base_url)
                 thrd.start()
 
         result = []
         mongo_result = Db.books.find({'tunnel': {"$in" : active_tunnels}})
         total_num = mongo_result.count()
+        if offset > total_num:
+            offset = total_num
         toolbar_authors = sorted(mongo_result.distinct('authors'))
         toolbar_titles = sorted(mongo_result.distinct('title_sort'))
         if total_num == 0:

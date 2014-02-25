9dcc48d74963dc97cca6f0938e4c41100575d45f
Marcell Mars <ki.ber@kom.uni.st>
2014-02-25 20:41:28 +0100

    build docker from inside the image.

---
+++ server_side/dockerfiles/build_mongodb_in_ubuntu.sh
@@ -0,0 +1,8 @@
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/10gen.list
+dpkg-divert --local --rename --add /sbin/initctl
+ln -s /bin/true /sbin/initctl
+rm /var/lib/apt/lists/* -vf
+apt-get update
+apt-get install mongodb-10gen
+mkdir -p /data/db

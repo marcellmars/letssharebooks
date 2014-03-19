bc8e421b87e88ae8d6c944164e80388048f9520e
Marcell Mars <ki.ber@kom.uni.st>
2014-03-19 14:14:51 +0100

    added openssl package to motw docker image

--- server_side/dockerfiles/memoryoftheworld/build_lsb_in_ubuntu.sh
+++ server_side/dockerfiles/memoryoftheworld/build_lsb_in_ubuntu.sh
@@ -17,7 +17,7 @@ echo "deb http://archive.ubuntu.com/ubuntu quantal main universe restricted mult
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 3B4FE6ACC0B21F32
 apt-get update
 
-apt-get -y install python-pip openssh-server dnsmasq nginx iproute
+apt-get -y install python-pip openssh-server dnsmasq nginx iproute openssl ssl-cert ca-certificates
 
 pip install supervisor cherrypy requests pymongo simplejson jinja2
 

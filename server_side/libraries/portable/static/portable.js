360ba6a233fc11178c6fa2e2bb2cdbd40fda0467
Marcell Mars <ki.ber@kom.uni.st>
2014-04-06 00:31:31 +0200

    got portable to work. but still only after going next and then back.

--- server_side/libraries/portable/static/portable.js
+++ server_side/libraries/portable/static/portable.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
         console.log("+ + + + + + + get_book_string_parts + + + + + + + +");
         console.log(format);
         book.application_id = '';
-        file_path = book.format_metadata[format].path
+        var file_path = book.format_metadata[format].path
         directory_path = ""
         directory = file_path.split("/").slice(-3,-1)
         directory.forEach(function(el){directory_path += el + "/"});
@@ -23,31 +23,47 @@ $(document).ready(function () {
         console.log("+ + + + + + + gen_book_content + + + + + + + +");
         console.log(book);
         book.application_id = '';
+        format = book.formats[0]
+        file_path = book.format_metadata[format].path
+        directory_path = ""
+        directory = file_path.split("/").slice(-3,-1)
+        directory.forEach(function(el){directory_path += el + "/"});
+        file_cover = "cover.jpg";
+        file_opf = "metadata"
         return {
             'base_url': '',
             'book': book,
-            'book_title_stripped': ' ' + book.title.replace(/\?/g, ''),
+            'book_title_stripped': '',
             'authors': authors,
             'formats': formats,
             'get_cover': '',
             'get_opf' : '',
-            'portable_cover': 'COVER',
-            'portable_opf': 'OPF'
+            'portable_cover': directory_path + file_cover,
+            'portable_opf': directory_path + file_opf
         }
     };
 
     window.gen_book_modal = function (base_url, book, formats) {
         console.log("+ + + + + + + gen_book_modal + + + + + + + +");
         book.application_id = '';
+        console.log(formats);
+        format = book.formats[0]
+        file_path = book.format_metadata[format].path
+        directory_path = ""
+        directory = file_path.split("/").slice(-3,-1)
+        directory.forEach(function(el){directory_path += el + "/"});
+        file_cover = "cover.jpg";
+        file_opf = "metadata"
+
         return { 
             'base_url': '',
             'book': book,
-            'book_title_stripped': ' ' + book.title.replace(/\?/g, ''),
+            'book_title_stripped': '',
             'formats': formats,
             'get_cover': '',
             'get_opf': '',
-            'portable_cover': 'cover.jpg',
-            'portable_opf': '',
+            'portable_cover': directory_path + file_cover,
+            'portable_opf': directory_path + file_opf,
         }
     };
 });

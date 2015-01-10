cdd19c22aa0df62508280b602cc500d056dff449
Marcell Mars <ki.ber@kom.uni.st>
2015-01-10 20:19:57 +0100

    set/unset now removes lines from dnsmasq so dnsmasq can continue handle remote motw requests after unset.

--- memoryoftheworld/set_local_env.py
+++ memoryoftheworld/set_local_env.py
@@ -3,8 +3,9 @@
 
 import subprocess
 
-hosts = open("/etc/hosts", "r").readlines()
-dmsq = open("/etc/dnsmasq.d/local", "r").readlines()
+#------------------------------------------------------------------------------
+#- getting ip addresses from dockers  -----------------------------------------
+
 docker_ips = {}
 for dckr in ["library", "mongodb", "nginx", "prosody", "sshd"]:
     docker_ips[dckr] = subprocess.check_output(["docker",
@@ -13,6 +14,10 @@ for dckr in ["library", "mongodb", "nginx", "prosody", "sshd"]:
                                                 "'{{ .NetworkSettings.IPAddress }}'",
                                                 "memoryoftheworld_{}_1".format(dckr)])[1:-2]
 
+#------------------------------------------------------------------------------
+#- add motw nginx ip address to  dnsmasq  -------------------------------------
+
+dmsq = open("/etc/dnsmasq.d/local", "r").readlines()
 init_pass = True
 for n,d in enumerate(dmsq):
     if d.startswith("address=/memoryoftheworld.org/"):
@@ -22,6 +27,12 @@ for n,d in enumerate(dmsq):
 if init_pass:
         dmsq.append("address=/memoryoftheworld.org/{}\n".format(docker_ips['nginx']))
 
+open("/etc/dnsmasq.d/local", "w").writelines(dmsq)
+
+#------------------------------------------------------------------------------
+#- add motw docker lines to /etc/hosts  ---------------------------------------
+
+hosts = open("/etc/hosts", "r").readlines()
 for n,d in enumerate(hosts):
     if "memoryoftheworld.org" in d:
         hosts.pop(n)
@@ -32,8 +43,15 @@ for i in ["xmpp", "anon", "conference"]:
 hosts.append("127.0.0.1 memoryoftheworld.org\n")
 
 open("/etc/hosts", "w").writelines(hosts)
-open("/etc/dnsmasq.d/local", "w").writelines(dmsq)
+
+#------------------------------------------------------------------------------
+#- set/etc/resolv.conf to be handled by dnsmasq -------------------------------
+
 open("/etc/resolv.conf", "w").write("nameserver 127.0.0.1\n")
+
+#------------------------------------------------------------------------------
+#- add iptables rule so remote/production motw server is blocked  -------------
+
 subprocess.call('iptables -A INPUT -s 82.221.106.120/32 -m comment --comment motw -j DROP', shell=True)
 subprocess.call(['service', 'dnsmasq', 'restart'])
 
--- memoryoftheworld/unset_local_env.py
+++ memoryoftheworld/unset_local_env.py
@@ -3,6 +3,17 @@
 
 import subprocess
 
+#------------------------------------------------------------------------------
+#- remove all motw iptables rules  --------------------------------------------
+
+rules = subprocess.check_output(['iptables', '-S'])
+for rule in rules.split("\n"):
+    if "motw" in rule:
+        subprocess.call('iptables {}'.format(rule.replace("-A", "-D")), shell=True)
+
+#------------------------------------------------------------------------------
+#- remove all motw lines from /etc/hosts  -------------------------------------
+
 hosts = open("/etc/hosts", "r").readlines()
 
 hosts_lines = []
@@ -11,12 +22,21 @@ for host in hosts:
         hosts_lines.append(host)
 
 open("/etc/hosts", "w").writelines(hosts_lines)
+
+#------------------------------------------------------------------------------
+#- set /etc/resolv.conf back to external dns  ---------------------------------
+#---* some systems will bring back (127.0.0.1) when dnsmasq restarts ----------
+
 open("/etc/resolv.conf", "w").write("nameserver 8.8.8.8\n")
 
-rules = subprocess.check_output(['iptables', '-S'])
-for rule in rules.split("\n"):
-    if "motw" in rule:
-        subprocess.call('iptables {}'.format(rule.replace("-A", "-D")), shell=True)
+#------------------------------------------------------------------------------
+#- remove motw docker line from /etc/dnsmasq.d/local  -------------------------
 
+dmsq = open("/etc/dnsmasq.d/local", "r").readlines()
+dms_lines = []
+for dms in dmsq:
+    if not dms.startswith("address=/memoryoftheworld.org/"):
+        dms_lines.append(dms)
+
+open("/etc/dnsmasq.d/local", "w").writelines(dms_lines)
 subprocess.call(['service', 'dnsmasq', 'restart'])
-

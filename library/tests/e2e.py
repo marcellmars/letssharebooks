01ae61c57ae91640c09b72caf7e101b48a63209c
klemo <klemo.vladimir@gmail.com>
2015-02-22 18:23:08 +0100

    webapp: more tests

--- library/tests/e2e.py
+++ library/tests/e2e.py
@@ -1,21 +1,24 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 
 class TestLSBWebApp(unittest.TestCase):
 
     def setUp(self):
+        #self.base_url = 'http://localhost:4321'
+        self.base_url = 'https://library.memoryoftheworld.org'
         self.driver = webdriver.Firefox()
 
     def test_initial_load(self):
-        self.driver.get('http://localhost:4321')
+        self.driver.get(self.base_url)
         self.assertEqual(self.driver.title, 'memory of the world library')
         # assert that there are some books on the site initially
         books = self.driver.find_elements_by_class_name('cover')
         self.assertGreater(len(books), 0)
 
     def test_book_modal(self):
-        self.driver.get('http://localhost:4321')
+        self.driver.get(self.base_url)
         # there should be no displayed modals initially
         displayed_modals = [i for i in self.driver.find_elements_by_id('book-modal') if i.is_displayed()]
         self.assertEqual(len(displayed_modals), 0)
@@ -26,8 +29,22 @@ class TestLSBWebApp(unittest.TestCase):
         displayed_modals = [i for i in self.driver.find_elements_by_id('book-modal') if i.is_displayed()]
         self.assertEqual(len(displayed_modals), 1)
 
+    # def test_book_modal_navigation(self):
+    #     self.driver.get(self.base_url)
+    #     # locate first book and click it
+    #     first_book = self.driver.find_element_by_class_name('cover')
+    #     first_book.find_element_by_class_name('more_about').click()
+    #     # there should be single modal displayed
+    #     displayed_modals = [i for i in self.driver.find_elements_by_id('book-modal') if i.is_displayed()]
+    #     self.assertEqual(len(displayed_modals), 1)
+    #     # nagivate to left modal
+    #     displayed_modals[0].send_keys(Keys.LEFT)
+    #     # there should be no modals displayed
+    #     displayed_modals = [i for i in self.driver.find_elements_by_id('book-modal') if i.is_displayed()]
+    #     self.assertEqual(len(displayed_modals), 0)
+
     def test_paging(self):
-        self.driver.get('http://localhost:4321')
+        self.driver.get(self.base_url)
         # previous page is not active initially
         prev_page = self.driver.find_element_by_class_name('prev_page')
         self.assertIn('not-active', prev_page.get_attribute('class'))
@@ -41,6 +58,57 @@ class TestLSBWebApp(unittest.TestCase):
         self.assertIn('active', prev_page2.get_attribute('class'))
         # ... and url should contain "page=2"
         self.assertGreater(self.driver.current_url.find('page=2'), -1)
+
+    def test_search(self):
+        
+        def form_clear(form):
+            form['title'].clear()
+            form['author'].clear()
+            form['search_all'].clear()
+            form['librarian'].select_by_value('')
+            
+        self.driver.get(self.base_url)
+        form = {}
+        form['search'] = self.driver.find_element_by_id('search')
+        form['title'] = self.driver.find_element_by_id('titles')
+        form['author'] = self.driver.find_element_by_id('authors')
+        form['search_all'] = self.driver.find_element_by_id('search_all')
+        form['librarian'] = Select(self.driver.find_element_by_id('librarian'))
+        
+        # search by TITLE
+        # search for all books for library 0
+        form_clear(form)
+        form['title'].send_keys('MCL0')
+        form['search'].click()
+        books = self.driver.find_elements_by_class_name('cover')
+        self.assertGreater(len(books), 0)
+        # search for all books for library 1
+        form_clear(form)
+        form['title'].send_keys('MCL1')
+        form['search'].click()
+        books = self.driver.find_elements_by_class_name('cover')
+        self.assertGreater(len(books), 0)
+
+        # search by AUTHOR
+        form_clear(form)
+        form['author'].send_keys('John The First')
+        form['search'].click()
+        books = self.driver.find_elements_by_class_name('cover')
+        self.assertEqual(len(books), 1)
+
+        # search by SEARCH_ALL
+        form_clear(form)
+        form['search_all'].send_keys('the')
+        form['search'].click()
+        books = self.driver.find_elements_by_class_name('cover')
+        self.assertGreater(len(books), 0)
+
+        # search by LIBRARIAN
+        form_clear(form)
+        form['librarian'].select_by_value('Joseph Of Byzantium')
+        form['search'].click()
+        books = self.driver.find_elements_by_class_name('cover')
+        self.assertGreater(len(books), 0)
 
     def tearDown(self):
         self.driver.close()

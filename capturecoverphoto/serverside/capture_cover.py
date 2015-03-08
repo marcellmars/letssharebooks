7fcafc7fe2845db9aa30c237230bb17df20b9fe3
Marcell Mars <ki.ber@kom.uni.st>
2015-03-08 18:51:13 +0000

    initial support for capturing calibre covers via gphoto2

--- capturecoverphoto/serverside/capture_cover.py
+++ capturecoverphoto/serverside/capture_cover.py
@@ -6,36 +6,42 @@ import shutil
 import posixpath
 import urllib
 import mimetypes
-import subprocess
-import time
+import piggyphoto as pp
 
-MOCK = True
+C = pp.camera()
+C.leave_locked()
 
-class HTTPHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+class HTTPHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):       
     def copyfile(self, source, outputfile):
         shutil.copyfileobj(source, outputfile)
-
-    def trigger_camera(self):
-        #if MOCK: return True
-        tc = subprocess.Popen(['gphoto2',
-                               '--auto-detect',
-                               '--capture-image-and-download',
-                               '--force-overwrite',
-                               '--set-config',
-                               'autofocusdrive=0',
-                               '--filename',
-                               '/tmp/cover.jpg'])
-        while tc.poll() is None:
-            time.sleep(0.1)
-        if tc.returncode == 0:
-            return True
-        else:
-            return False
-  
-    def capture_photo(self):
-        if not self.trigger_camera():
-            return False
         
+    def preview(self):
+        C.capture_preview("/tmp/preview.jpg")
+        try:
+            f = open("/tmp/preview.jpg", 'rb')
+        except IOError:
+            self.send_error(404, "File not found")
+            return None
+
+        if not f:
+            self.send_error(404, "File not found")
+            return None
+        
+        self.send_response(200)
+        self.send_header("Content-type", "image/jpeg")
+        fs = os.fstat(f.fileno())
+        self.send_header("Content-Length", str(fs[6]))
+        self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+        self.send_header('Cache-Control', 'no-cache, no-store, must-revalidate')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
+        self.end_headers()
+        self.copyfile(f, self.wfile)
+        f.close()
+        
+    def capture_photo(self):
+        C.capture_image("/tmp/cover.jpg")
         try:
             f = open("/tmp/cover.jpg", 'rb')
         except IOError:
@@ -57,9 +63,10 @@ class HTTPHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         return
        
     def do_GET(self):
-        os.chdir("/tmp/")
         if self.path[1:] in "capture":
             self.capture_photo()
+        elif self.path[1:] in "preview":
+            self.preview()
 
     def translate_path(self, path):
        # abandon query parameters

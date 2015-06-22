5e047d787b41da2d809ad49af3ba3cdde40f43eb
Marcell Mars <ki.ber@kom.uni.st>
2015-06-22 19:52:06 +0200

    i hate when git is too smart for me

--- memoryoftheworld/local_env.py
+++ memoryoftheworld/local_env.py
@@ -1,2 +1,4 @@
-MOTW_HOME = "*ChangeMe*"
-MOTW_PERSISTENCE = "*ChangeMe*"
+MOTW_HOME = "/home/m/devel"
+MOTW_WORDPRESS_HOME = "/home/m/devel/letssharebooks"
+MOTW_PERSISTENCE = "/home/mnt/docker-persistence"
+RSYNC_DIRECTORY = "/home/m/devel/letssharebooks/memoryoftheworld/rsync"

717416180de4dc95affbcd4f66236801efaa8f9e
Marcell Mars <ki.ber@kom.uni.st>
2015-08-25 00:24:45 +0200

    synced common.js and local_calibre.js

--- library/static/local_calibre.js
+++ library/static/local_calibre.js
@@ -5,6 +5,10 @@ var localCalibre = (function () {
     var baseGIFLocation = 'http://localhost:56665/';
     /** connects to local calibre server with callback(success=true|false) */
     var connect = function(callback) {
+        if (document.location.origin === "file://") {
+            callback(false);
+            return;
+        }
         // try to locate 0.gif
         $('<img src="' + baseGIFLocation + '0.gif" />')
         // 0.gif is available
@@ -18,21 +22,19 @@ var localCalibre = (function () {
                     if (localLibrary) { return };
 
                     local_library_uuid = catalog.library_uuid;
-
                     if (catalog.library_uuid.indexOf("p::") == 0) {
                         local_library_uuid = catalog.library_uuid.slice(3,-3);
-                    };
+                    }
 
                     var lib_gif = ['<img src="',
                                    baseGIFLocation,
                                    local_library_uuid,
                                    '.gif" />'].join('');
-
                     $(lib_gif).load(function() {
                         localLibrary = catalog.library_uuid;
                         callback(true);
                     })
-                    .on('error', function() {
+                    .error(function() {
                         count_local_gif_err += 1;
                         if (count_local_gif_err === catalogs.length) {
                             callback(false);
@@ -42,7 +44,7 @@ var localCalibre = (function () {
             });
 	      })
         // 0.gif is not available
-        .on('error', function() {
+        .error(function() {
             callback(false);
 	      });
     };

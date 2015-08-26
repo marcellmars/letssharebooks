17ba02e41f2f6e3d04f1b610fcc7ec9e7de79785
Marcell Mars <ki.ber@kom.uni.st>
2015-08-26 02:00:36 +0200

    we should sanitize html...

--- memoryoftheworld/library/build_library.sh
+++ memoryoftheworld/library/build_library.sh
@@ -2,6 +2,6 @@
 
 apt-get update
 
-apt-get -y install iproute build-essential python-dev
-pip install cherrypy requests simplejson jinja2
+apt-get -y install iproute build-essential python-dev libxml2-dev
+pip install cherrypy requests simplejson jinja2 lxml
 pip install https://github.com/mongodb/mongo-python-driver/archive/3.0.1.tar.gz

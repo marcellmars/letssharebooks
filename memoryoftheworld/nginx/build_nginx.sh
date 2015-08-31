4bba9c516fed2e63fd95e0914d4936a75816be48
Marcell Mars <ki.ber@kom.uni.st>
2015-08-31 02:33:54 +0200

    deleted all build_x docker scripts

--- memoryoftheworld/nginx/build_nginx.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-apt-get update
-
-apt-get -y install nginx
-echo "daemon off;" >> /etc/nginx/nginx.conf
--- memoryoftheworld/rsync/build_rsync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-apt-get update
-apt-get -y install rsync
-
-mkdir /etc/rsync.d/
-touch /etc/rsync.d/rsyncd.secrets
-chown root /etc/rsync.d/rsyncd.secrets

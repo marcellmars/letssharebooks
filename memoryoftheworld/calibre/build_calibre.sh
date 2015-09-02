647f326853cb1e5375ba60d431515c513d37084f
Marcell Mars <ki.ber@kom.uni.st>
2015-09-02 16:08:10 +0200

    calibre docker updated to the same way of logging

--- memoryoftheworld/calibre/build_calibre.sh
+++ memoryoftheworld/calibre/build_calibre.sh
@@ -14,5 +14,5 @@ chmod +x /usr/local/bin/run.sh
 apt-get update
 
 apt-get -y install python wget xz-utils xdg-utils imagemagick openssh-client
-wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py |PYTHONIOENCODING="utf-8" python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
+wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | PYTHONIOENCODING="utf-8" python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
 

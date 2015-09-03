7d938e4ba6912ab0d8f3b541310880be4beb1ddd
Marcell Mars <ki.ber@kom.uni.st>
2015-09-03 14:22:14 +0200

    dhparam.pem is important part...

--- memoryoftheworld/library/build_library.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-apt-get update
-
-apt-get -y install iproute build-essential python-dev python-lxml
-pip install cherrypy requests simplejson jinja2
-pip install https://github.com/mongodb/mongo-python-driver/archive/3.0.1.tar.gz

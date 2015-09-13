dbc427afeb61a12d7154e0d2812aa852274eb8de
Marcell Mars <ki.ber@kom.uni.st>
2015-09-13 19:59:07 +0200

    major rearrangement of folders and files
    this shouldn't be done like this. i know. will survive...

---
+++ memoryoftheworld/library/webapp/send2dokr.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp libraries.py ~/Downloads/dokrfs/var/www/libraries/libraries.py
+cp static/style.css ~/Downloads/dokrfs/var/www/libraries/static
+cp static/libraries.js ~/Downloads/dokrfs/var/www/libraries/static
+cp templates/index.html ~/Downloads/dokrfs/var/www/libraries/templates
--- memoryoftheworld/setup_docker-compose.py
+++ memoryoftheworld/setup_docker-compose.py
@@ -1,7 +1,7 @@
 import local_env as G
 
 LSBM = '/letssharebooks/memoryoftheworld/'
-for i in ["docker-compose", "calibre"]:
+for i in ["docker-compose", "motw-compose", "calibre"]:
     with open("{}{}{}_template.yml".format(G.MOTW_HOME, LSBM, i)) as f:
         with open("{}{}{}.yml".format(G.MOTW_HOME, LSBM, i), "w") as g:
             g.write(f.read().replace('''${MOTW_HOME}''',

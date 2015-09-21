82ed39d17a37e8ffe77fc35de1319aa58926aafb
Marcell Mars <ki.ber@kom.uni.st>
2015-09-21 05:32:12 +0200

    tunnels got early into integers + get checked
    in socket_server.py. this whole thing is a mess and it should be
    rewritten soon..

--- memoryoftheworld/ssh/socket_server.py
+++ memoryoftheworld/ssh/socket_server.py
@@ -9,11 +9,19 @@ def get_tunnel_ports(login="tunnel"):
     tunnel_uid = subprocess.check_output(["grep", "^{0}:"
                                           .format(login),
                                           "/etc/passwd"]).split()[0].split(":")[2]
-    active_tunnel_ports = [n.split(":")[1].split(" ")[0]
+    tunnel_ports = [n.split(":")[1].split(" ")[0]
                            for n in subprocess.check_output(['ss', '-4el']).split('\n')
                            if n.find('uid:{} '.format(tunnel_uid)) != -1]
 
     # print("active tunnel ports: {}".format(active_tunnel_ports))
+    active_tunnel_ports = []
+    for a in tunnel_ports:
+        try:
+            i = int(a)
+            if type(i) == int and i > 1023 and i < 49024:
+                active_tunnel_ports.append(i)
+        except:
+            pass
     return active_tunnel_ports
 
 
@@ -24,9 +32,10 @@ class MyTCPServer(SocketServer.ThreadingTCPServer):
 class MyTCPServerHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         try:
-            data = json.loads(self.request.recv(1024).strip())
-            self.request.sendall(json.dumps(get_tunnel_ports()))
-            #self.request.sendall(json.dumps([123,345,456]))
+            req = json.loads(self.request.recv(1024).strip())
+            if req['get'] == 'active_tunnel_ports':
+                self.request.sendall(json.dumps(get_tunnel_ports()))
+                #self.request.sendall(json.dumps([123,345,456]))
         except Exception, e:
             print("Upss:", e)
 
